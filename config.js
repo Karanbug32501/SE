1e2dbf32e9faef245dae7ae2c4e71e989b270de2
Karanbug32501 <karan.arora@bughunters.io>
2025-06-19 18:58:13 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Rough water web URL
-const APP_URL = "https://staging.seospace.co";
-const RELOAD_PAGE_HOSTNAME = ["staging.seospace.co", "app.seospace.co"];
-const COOKIE_DOMAIN_NAME = APP_URL;
-const COOKIE_NAME = "RoughWaterUserLogging";
